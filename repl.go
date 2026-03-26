e60c9c6adf22b07091b5be419b1aa54e743de807
brentjolicoeur <brentjolicoeur@gmail.com>
2026-03-26 14:24:06 -0400

    refactored to new command files

--- repl.go
+++ repl.go
@@ -7,6 +7,38 @@ import (
 	"strings"
 )
 
+func startREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			cleanedInput := cleanInput(input)
+			if len(cleanedInput) == 0 {
+				continue
+			}
+			commandName := cleanedInput[0]
+			cmd, exists := getCommands()[commandName]
+			if exists {
+				err := cmd.callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+				continue
+			} else {
+				fmt.Println("Unknown command")
+				continue
+			}
+		} else {
+			if scanner.Err() != nil {
+				fmt.Print(scanner.Err())
+			}
+			break
+		}
+	}
+}
+
 type cliCommand struct {
 	name        string
 	description string
@@ -19,61 +51,17 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func startREPL() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	var supportedCommands map[string]cliCommand
-	supportedCommands = map[string]cliCommand{
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback: func() error {
-				return commandHelp(supportedCommands)
-			},
-		},
 	}
-
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			input := scanner.Text()
-			cleanedInput := cleanInput(input)
-			if len(cleanedInput) == 0 {
-				continue
-			}
-			firstWord := cleanedInput[0]
-			if cmd, ok := supportedCommands[firstWord]; ok {
-				err := cmd.callback()
-				if err != nil {
-					fmt.Println(err)
-				}
-			} else {
-				fmt.Println("Unknown command")
-			}
-		} else {
-			if scanner.Err() != nil {
-				fmt.Print(scanner.Err())
-			}
-			break
-		}
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-func commandHelp(commands map[string]cliCommand) error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Print("Usage:\n\n")
-	for _, cmd := range commands {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	return nil
 }
