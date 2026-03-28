ece4d7a12db2e14cac2c275d32c4462df0b03269
brentjolicoeur <brentjolicoeur@gmail.com>
2026-03-28 19:31:48 -0400

    added check for empty pokedex

--- command_pokedex.go
+++ command_pokedex.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func commandPokedex(cfg *config, args []string) error {
+	if len(cfg.caughtPokemon) == 0 {
+		return errors.New("You haven't caught any pokemon!")
+	}
 	fmt.Println("Your Pokedex:")
 	for pokemon := range cfg.caughtPokemon {
 		fmt.Printf(" - %v\n", pokemon)
